2b2a61052788a1503dd96de356e6248422a98c26
Nitesh223 <nitesh725025@gmail.com>
2025-06-02 10:36:36 +0530

    images

--- E Commerce.js
+++ E Commerce.js
@@ -1,4 +1,13 @@
-var carousel_images = ["assets/HP_Rotating_Apple_MainBanner_26May2025_b9slnz.webp", "assets/HP_Rotating_CromaACs_30May2025_v2dclq.webp", "assets/HP_Rotating_Fans_30May2025_gdprkg.webp", "assets/HP_Rotating_HD_30May2025_w7hgxr.webp", "assets/HP_Rotating_NothingPhone2aPlus_30May2025_v4a09z.webp", "assets/HP_Rotating_Ref_30May2025_lixpah.webp", "assets/HP_Rotating_SonySB_30May2025_ayevaq.webp", "assets/HP_Rotating_SW_30May2025_gy0zed.webp"]
+var carousel_images = [
+    "assets/home-carousel-images/HP_Rotating_Apple_MainBanner_26May2025_b9slnz.webp",
+    "assets/home-carousel-images/HP_Rotating_TVs_01June2025_ffyznc.webp",
+    "assets/home-carousel-images/HP_Rotating_CromaACs_30May2025_v2dclq.webp",
+    "assets/home-carousel-images/HP_Rotating_HD_30May2025_w7hgxr.webp",
+    "assets/home-carousel-images/HP_Rotating_NothingPhone2aPlus_30May2025_v4a09z.webp",
+    "assets/home-carousel-images/HP_Rotating_Ref_30May2025_lixpah.webp",
+    "assets/home-carousel-images/HP_Rotating_SonySB_30May2025_ayevaq.webp",
+    "assets/home-carousel-images/HP_Rotating_SW_30May2025_gy0zed.webp"
+]
 
 var current = 0
 function show_image() {
